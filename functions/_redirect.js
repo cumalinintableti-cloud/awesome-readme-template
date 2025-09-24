1ac01cc60db057fd598b0fe19a7a4a1f1848681b
dildo <cumalinintableti@gmail.com>
2025-09-24 08:45:40 +0300

    Delete functions directory

--- functions/_redirect.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest(context) {
-  const newUrl = "https://turkgdpsdl.pages.dev"; // Buraya Pages URL'ini yaz
-  return Response.redirect(newUrl, 301); // 301 = kalıcı yönlendirme
-}
